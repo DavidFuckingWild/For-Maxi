083490850cf995c7358d4443960f0c4a5bd552f2
DavidFuckingWild <46425840+DavidFuckingWild@users.noreply.github.com>
2020-01-31 23:25:19 +0100

    Update lambda-functions.js

--- chapters/lambda-functions.js
+++ chapters/lambda-functions.js
@@ -1,12 +1,12 @@
 /*
     I advise you to use the so called 'lambda-expressions' instead of the normal function-keyword.
     Why?
-    Because functions have (unlike in other programming languages) a this-keyword and a __prototype__.
-    What they are, how and when to use them will I explain later. 
+    Because functions have (unlike in other programming languages) a this-keyword and a __proto__-property.
+    What they are, how and when to use them will I explain later in the last chapter. 
     The important thing is that the this-keyword can be used in diffrent contextes and that can easely lead to 
     unintended bugs or ugly programming patterns. Especially beginners make mistakes because of that.
     The advantage of 'lambda-expressions' is that they are exactly like functions, but they dont have the this-keyword
-    and the __prototype__.
+    and the __proto__-property.
 */
 
 /*--------------------------------------------------------*/
@@ -24,11 +24,12 @@ lambda('Hej världen!')
 const lambdaClon = lambda
 // also you can assign your lambda-function to an other variable
 // ATTENTION! if you use brackets you execute the function, 
-// you want not do that by an assigment.
+// you dont want to do that by an assigment.
 
 lambdaClon('Hoila Maxi, wie geahtsn?')
 
 /*--------------------------------------------------------*/
+// Some example how you can rewrite a function as lambda expression
 
 function functionName() {
     //do something
